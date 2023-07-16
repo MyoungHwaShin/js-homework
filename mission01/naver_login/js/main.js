4c40b0e3d520cb80ebdf34b61a75f63b2c7d23b8
MyoungHwaShin <shinmyounghwa92@gmail.com>
2023-07-16 16:53:58 +0900

    로그인 폼 입력값에 따른 페이지 이동 / 경고창 띄우기

---
+++ mission01/naver_login/js/main.js
@@ -0,0 +1,70 @@
+
+const user = {
+  id:'asd@naver.com',
+  pw:'spdlqj123!@'
+}
+
+
+function emailReg(text){
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(String(text).toLowerCase())
+}
+
+
+function pwReg(text){
+  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+  return re.test(String(text).toLowerCase());
+}
+
+const email = document.querySelector('.user-email-input');
+const pw = document.querySelector('.user-password-input');
+const button = document.querySelector('.btn-login');
+
+let emailPass = false;
+let pwPass = false;
+
+email.addEventListener('input',function() {
+  if (emailReg(this.value)){
+    this.classList.remove('is--invalid');
+    emailPass = true;
+  }else {
+    this.classList.add('is--invalid');
+
+    emailPass = false;
+
+  }
+})
+
+pw.addEventListener('input',function() {//이벤트에 인풋 쓰기
+  if (pwReg(this.value)){
+    this.classList.remove('is--invalid');
+    pwPass = true;
+  }else {
+    this.classList.add('is--invalid');
+
+    pwPass = false;
+
+  }
+
+})
+
+
+button.addEventListener('click',()=>{
+  
+  if( email.value === user.id && pw.value === user.pw) {
+    window.location.href = 'welcome.html';
+
+  }else {
+    alert('아이디 또는 패스워드를 확인 해주세요.');
+  }
+  
+})
+
+
+
+
+
+
+
+
